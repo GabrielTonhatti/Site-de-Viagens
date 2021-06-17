059d7ce1d54825fb56df68f2e6842558fd2fa75e
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2021-06-17 17:53:45 -0300

    Cards for desktop

--- js/cards.js
+++ js/cards.js
@@ -1,29 +1,44 @@
 const slides = document.querySelectorAll('.card');
 const nextButton = document.querySelector('.button--next');
-const prevButton = document.querySelector('.button--prev')
+const prevButton = document.querySelector('.button--prev');
+let windowWidth = window.innerWidth;
 
 console.log(slides)
-const lastSlideIndex =  slides.length - 1
+const lastSlideIndex = slides.length - 1
 let currentSlideIndex = 0;
 
-const manipulateSlideClasses = correctSlideIndex => {
-    
-    slides.forEach(slide => slide.classList.remove('active'));
-    slides[correctSlideIndex].classList.add('active');
+if (windowWidth < 768) {
+    const manipulateSlideClasses = correctSlideIndex => {
+
+        slides.forEach(slide => slide.classList.remove('active'));
+        slides[correctSlideIndex].classList.add('active');
+
+    }
+
+    nextButton.addEventListener('click', () => {
+
+        const correctSlideIndex = currentSlideIndex === lastSlideIndex ? currentSlideIndex = 0 : ++currentSlideIndex;
+
+        manipulateSlideClasses(correctSlideIndex);
+    });
+
+    prevButton.addEventListener('click', () => {
+
+        const correctSlideIndex = currentSlideIndex === 0 ? currentSlideIndex = lastSlideIndex : --currentSlideIndex;
+
+        manipulateSlideClasses(correctSlideIndex);
+
+    })
 
 }
 
-nextButton.addEventListener('click', () => {
-
-    const correctSlideIndex = currentSlideIndex === lastSlideIndex ? currentSlideIndex = 0 : ++currentSlideIndex
-
-    manipulateSlideClasses(correctSlideIndex)
-});
-
-prevButton.addEventListener('click', () => {
-
-    const correctSlideIndex = currentSlideIndex === 0 ? currentSlideIndex = lastSlideIndex : --currentSlideIndex
-
-    manipulateSlideClasses(correctSlideIndex)
-
-})
+if (windowWidth >= 768 && windowWidth < 1440) {
+    slides[1].classList.add('active');
+} else if (windowWidth >= 1440 && windowWidth < 2560) {
+    slides[1].classList.add('active');
+    slides[2].classList.add('active');
+} else if (windowWidth >= 2560) {
+    slides[1].classList.add('active');
+    slides[2].classList.add('active');
+    slides[3].classList.add('active');
+}
