bd832697af1c56b33603eeefbdceeacf3a679a0d
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2021-06-20 13:35:21 -0300

    Cards for ferias

--- js/cards.js
+++ js/cards.js
@@ -1,44 +1,91 @@
-const slides = document.querySelectorAll('.card-mais-visitados');
-const nextButton = document.querySelector('.button--next-1');
-const prevButton = document.querySelector('.button--prev-1');
+const slides1 = document.querySelectorAll('.card-mais-visitados');
+const slides2 = document.querySelectorAll('.card-ferias')
+const nextButton1 = document.querySelector('.button--next-1');
+const prevButton1 = document.querySelector('.button--prev-1');
+const nextButton2 = document.querySelector('.button--next-2');
+const prevButton2 = document.querySelector('.button--prev-2');
 let windowWidth = window.innerWidth;
 
-console.log(slides)
-const lastSlideIndex = slides.length - 1
+console.log(slides1)
+console.log(slides2)
+const lastSlideIndex = slides1.length - 1
 let currentSlideIndex = 0;
+let currentSlideIndex2 = 0;
 
 if (windowWidth < 768) {
-    const manipulateSlideClasses = correctSlideIndex => {
+    const slidesMaisVisitados = correctSlideIndex => {
 
-        slides.forEach(slide => slide.classList.remove('active'));
-        slides[correctSlideIndex].classList.add('active');
+        slides1.forEach(slide => slide.classList.remove('active'));
+        slides1[correctSlideIndex].classList.add('active');
 
     }
 
-    nextButton.addEventListener('click', () => {
+    nextButton1.addEventListener('click', () => {
 
         const correctSlideIndex = currentSlideIndex === lastSlideIndex ? currentSlideIndex = 0 : ++currentSlideIndex;
 
-        manipulateSlideClasses(correctSlideIndex);
+        slidesMaisVisitados(correctSlideIndex);
     });
 
-    prevButton.addEventListener('click', () => {
+    prevButton1.addEventListener('click', () => {
 
         const correctSlideIndex = currentSlideIndex === 0 ? currentSlideIndex = lastSlideIndex : --currentSlideIndex;
 
-        manipulateSlideClasses(correctSlideIndex);
+        slidesMaisVisitados(correctSlideIndex);
 
     })
 
+    const slidesFerias = correctSlideIndex => {
+
+        slides2.forEach(slide => slide.classList.remove('active'));
+        slides2[correctSlideIndex].classList.add('active');
+
+    }
+
+    nextButton2.addEventListener('click', () => {
+
+        const correctSlideIndex = currentSlideIndex2 === slides2.length - 1 ? currentSlideIndex2 = 0 : ++currentSlideIndex2;
+
+        slidesFerias(correctSlideIndex);
+    });
+
+    prevButton2.addEventListener('click', () => {
+
+        const correctSlideIndex = currentSlideIndex2 === 0 ? currentSlideIndex2 = slides2.length - 1 : --currentSlideIndex2;
+
+        slidesFerias(correctSlideIndex);
+
+    })
 }
 
 if (windowWidth >= 768 && windowWidth < 1440) {
-    slides[1].classList.add('active');
+    // Mais visitados
+    slides1[1].classList.add('active');
+
+    // Ferias
+    slides2[1].classList.add('active');
+
+
 } else if (windowWidth >= 1440 && windowWidth < 2560) {
-    slides[1].classList.add('active');
-    slides[2].classList.add('active');
+    // Mais visitados
+    slides1[1].classList.add('active');
+    slides1[2].classList.add('active');
+
+    // Ferias
+    slides2[1].classList.add('active');
+    slides2[2].classList.add('active');
+
+
 } else if (windowWidth >= 2560) {
-    slides[1].classList.add('active');
-    slides[2].classList.add('active');
-    slides[3].classList.add('active');
+    // Mais visitados
+    slides1[1].classList.add('active');
+    slides1[2].classList.add('active');
+    slides1[3].classList.add('active');
+
+    // Ferias
+    slides2[1].classList.add('active');
+    slides2[2].classList.add('active');
+    slides2[3].classList.add('active');
+
+
 }
